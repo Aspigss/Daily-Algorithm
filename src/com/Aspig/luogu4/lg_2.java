0810a3143fe33a714ac4646def0b6e203a79dbd2
Aspigss <2556784875@qq.com>
2026-04-03 13:30:40 +0800

    前缀和

---
+++ src/com/Aspig/luogu4/lg_2.java
@@ -0,0 +1,33 @@
+package com.Aspig.luogu4;
+
+import java.util.Scanner;
+
+public class lg_2 {
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int n = scanner.nextInt();
+	int d = scanner.nextInt();
+	int[] water = new int[n+1];
+     int[] S = new int[n+1];//Ç°×ººÍ	
+	for (int i = 1; i <=n; i++) {
+		water[i] = scanner.nextInt();
+	}
+	for (int i = 1; i <=n; i++) {
+		S[i] = S[i-1]+water[i];
+	}
+	int ans =0;
+	int r = d+1;
+	int l = 1;
+     for (int i = 0; i <n-d; i++) {
+    	int sum = S[r]-S[l-1];
+    	l++;
+    	r++;
+    	//for (int j = 1; j <=d+1; j++) { ±©Á¦!
+    			//sum = sum+water[j];
+    		//}
+    		
+    		ans = Math.max(ans, sum);
+	}
+     System.out.println(ans);
+}
+}
