bc765fba3302c6a6ead264cc1915df5102120a31
Aspigss <2556784875@qq.com>
2026-04-04 15:29:37 +0800

    递归

--- src/com/Aspig/luogu5/lg_7.java
+++ src/com/Aspig/luogu5/lg_7.java
@@ -1,12 +1,12 @@
 package com.Aspig.luogu5;
 
 import java.util.Scanner;
-
+//斐波那契
 public class lg_7 {
 	static int fb(int a)
 	{
-		if(a==1||a==2) return 1;
-		return fb(a-1)+fb(a-2);
+		if(a==1||a==2) return 1;//定界
+		return fb(a-1)+fb(a-2);//递归公式
 	}
 public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
---
+++ src/com/Aspig/luogu5/lg_8.java
@@ -0,0 +1,19 @@
+package com.Aspig.luogu5;
+
+import java.util.Scanner;
+
+public class lg_8 {
+	static int f(int m,int n)
+	{
+		if(m==0) return n+1;
+		if(n==0) return f(m-1, 1);
+		return f(m-1, f(m, n-1));
+	}
+public static void main(String[] args) {
+	int m,n;
+	Scanner scanner = new Scanner(System.in);
+	m = scanner.nextInt();
+	n =scanner.nextInt();
+   System.out.println(f(m, n));
+}
+}
