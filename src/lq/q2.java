a95aa3ebae5c5c19d45f81f0d9b1408e44b4f3db
Aspigss <2556784875@qq.com>
2026-04-05 20:43:31 +0800

    蓝桥

---
+++ src/lq/q2.java
@@ -0,0 +1,23 @@
+package lq;
+
+public class q2 {
+public static void main(String[] args) {
+	int n;
+	long a = 20250412L;
+	long b = 20240413L;
+	/*
+	 * n+a =kb
+	 * n+b%a==0
+	 */
+	int i =0;
+     while(true)
+      {
+       	  if((i*b-a+b)%a==0)
+       	  {
+       		  System.out.println(i*b-a);
+           	  break;
+       	  }
+       	  i++;
+       }
+}
+}
